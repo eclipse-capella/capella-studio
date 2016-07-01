e8ce0ae483078106ae599bb4fc372db6c9929a2e
Zendagui boubekeur <boubekeur.zendagui@obeo.fr>
2016-07-01 17:45:05 +0200

    [1051] Test of the Capella integration in Viewpoint DSL
    Bug: 1051
    Change-Id: I78343ec1800da7fb6a3955415a656c2f93b4fc27
    Signed-off-by: Zendagui boubekeur <boubekeur.zendagui@obeo.fr>

---
+++ vpdsl/tests/org.polarsys.capella.ad.viewpoint.dsl.tests/src/org/polarsys/capella/ad/viewpoint/dsl/tests/suite/AllTests.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+* Copyright (c) 2016 THALES GLOBAL SERVICES.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    Thales - initial API and implementation
+*******************************************************************************/
+
+package org.polarsys.capella.ad.viewpoint.dsl.tests.suite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.polarsys.capella.ad.viewpoint.dsl.tests.ta.CapellaTargetApplicationTests;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	CapellaTargetApplicationTests.class
+})
+public class AllTests {
+
+}
---
+++ vpdsl/tests/org.polarsys.capella.ad.viewpoint.dsl.tests/src/org/polarsys/capella/ad/viewpoint/dsl/tests/ta/CapellaTargetApplicationTests.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+* Copyright (c) 2016 THALES GLOBAL SERVICES.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    Thales - initial API and implementation
+*******************************************************************************/
+
+package org.polarsys.capella.ad.viewpoint.dsl.tests.ta;
+
+import org.eclipse.egf.portfolio.genchain.extension.ExtensionHelper;
+import org.eclipse.emf.ecore.EClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.polarsys.capella.ta.definition.CapellaTargetApplication;
+import org.polarsys.kitalpha.ad.ta.extension.TargetApplicationExtensionManager;
+
+import java.util.List;
+
+public class CapellaTargetApplicationTests {
+
+	private static final CapellaTargetApplication C_TA = CapellaTargetApplication.INSTANCE;
+	/**
+	 * Checking if the Target Application Capella is available in the platform
+	 */
+	@Test
+	public void checkCapellaTAExistence(){
+		String[] supportedTargetApplication = TargetApplicationExtensionManager.getSupportedTargetApplication();
+		boolean available = false;
+		for (int i = 0; i < supportedTargetApplication.length; i++) 
+		{
+			final String currentTA = supportedTargetApplication[i];
+			if (currentTA.equalsIgnoreCase("Capella"))
+			{
+				available = true;
+				break;
+			}
+		}
+		
+		assertEquals(true, available);
+	}
+	
+	/**
+	 * Checking if the NamedElement EClass is well accessible
+	 */
+	@Test
+	public void testGetBasicSemanticElement() {
+		List<EClass> basicSemanticElement = C_TA.getBasicSemanticElement();
+		assertEquals(1, basicSemanticElement.size());
+		
+		EClass eClass = basicSemanticElement.get(0);
+		assertEquals("NamedElement", eClass.getName());
+	}
+
+	/**
+	 * Checking if there are two default extension
+	 */
+	@Test
+	public void testGetGenerationExtensionHelpers() {
+		List<ExtensionHelper> helpers = C_TA.getGenerationExtensionHelpers();
+		assertEquals(true, helpers.size() >= 2);
+	}
+
+}
