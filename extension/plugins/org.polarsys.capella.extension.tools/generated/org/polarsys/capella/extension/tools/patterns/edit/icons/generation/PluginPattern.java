e3f48fa28700e3754dd10006033255fb13ab28aa
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2020-11-25 10:51:51 +0100

    [569054] Update copyright year on all modified files
    Change-Id: I6829a64650625be737f666e2541f990b97d9a8f3
    Signed-off-by: Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>

--- extension/plugins/org.polarsys.capella.extension.tools/generated/org/polarsys/capella/extension/tools/patterns/edit/icons/generation/PluginPattern.java
+++ extension/plugins/org.polarsys.capella.extension.tools/generated/org/polarsys/capella/extension/tools/patterns/edit/icons/generation/PluginPattern.java
@@ -208,7 +208,7 @@ public class PluginPattern extends org.eclipse.egf.emf.pattern.edit.Plugin {
 	protected void method_doGenerate(final StringBuffer stringBuffer, final PatternContext ctx) throws Exception {
 
 		/**
-		 * Copyright (c) 2002-2020 IBM Corporation and others.
+		 * Copyright (c) 2002-2010 IBM Corporation and others.
 		 * All rights reserved.   This program and the accompanying materials
 		 * are made available under the terms of the Eclipse Public License v1.0
 		 * which accompanies this distribution, and is available at
