836b9eb6c93f518285e3ecb40e4a8268794dfc2a
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2021-12-10 11:40:54 +0100

    #141 Fix CDO code generation customization
    Rely on the Capella Cdo Emf Generation for CDO code generation.
    
    Change-Id: I0000000000000000000000000000000000000000
    Signed-off-by: Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>

--- vpdsl/plugins/org.polarsys.capella.cdo.ta.definition/src/org/polarsys/capella/cdo/ta/definition/CDOForCapellaTargetApplication.java
+++ vpdsl/plugins/org.polarsys.capella.cdo.ta.definition/src/org/polarsys/capella/cdo/ta/definition/CDOForCapellaTargetApplication.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2020 Thales Global Services.
+ * Copyright (c) 2016, 2021 Thales Global Services.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0
@@ -13,8 +13,8 @@ package org.polarsys.capella.cdo.ta.definition;
 
 import java.util.List;
 
-import org.eclipse.egf.portfolio.genchain.cdo.CdoGenerationExtension;
 import org.eclipse.egf.portfolio.genchain.extension.ExtensionHelper;
+import org.polarsys.capella.extension.genchain.CapellaCdoGenerationExtension;
 import org.polarsys.capella.ta.definition.CapellaTargetApplication;
 
 
@@ -27,7 +27,7 @@ public class CDOForCapellaTargetApplication extends CapellaTargetApplication {
 	@Override
 	public List<ExtensionHelper> getGenerationExtensionHelpers() {
 		List<ExtensionHelper> generationExtensionHelpers = super.getGenerationExtensionHelpers();
-		generationExtensionHelpers.add(new CdoGenerationExtension());
+		generationExtensionHelpers.add(new CapellaCdoGenerationExtension());
 		return generationExtensionHelpers;
 	}
 
