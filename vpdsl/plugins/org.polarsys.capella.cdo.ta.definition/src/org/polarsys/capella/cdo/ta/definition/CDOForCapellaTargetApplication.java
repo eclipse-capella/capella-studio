17658250a60a9e4a6c99d78848d2e00cd82cbf85
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2021-12-10 11:40:54 +0100

    #141 Fix CDO code generation customization
    Rely on the Capella Cdo Emf Generation for CDO code generation.
    
    Change-Id: I0000000000000000000000000000000000000000
    Signed-off-by: Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>

--- vpdsl/plugins/org.polarsys.capella.cdo.ta.definition/src/org/polarsys/capella/cdo/ta/definition/CDOForCapellaTargetApplication.java
+++ vpdsl/plugins/org.polarsys.capella.cdo.ta.definition/src/org/polarsys/capella/cdo/ta/definition/CDOForCapellaTargetApplication.java
@@ -13,8 +13,8 @@ package org.polarsys.capella.cdo.ta.definition;
 
 import java.util.List;
 
-import org.eclipse.egf.portfolio.genchain.cdo.CdoGenerationExtension;
 import org.eclipse.egf.portfolio.genchain.extension.ExtensionHelper;
+import org.polarsys.capella.extension.genchain.CapellaCdoGenerationExtension;
 import org.polarsys.capella.ta.definition.CapellaTargetApplication;
 
 
@@ -27,7 +27,7 @@ public class CDOForCapellaTargetApplication extends CapellaTargetApplication {
 	@Override
 	public List<ExtensionHelper> getGenerationExtensionHelpers() {
 		List<ExtensionHelper> generationExtensionHelpers = super.getGenerationExtensionHelpers();
-		generationExtensionHelpers.add(new CdoGenerationExtension());
+		generationExtensionHelpers.add(new CapellaCdoGenerationExtension());
 		return generationExtensionHelpers;
 	}
 
